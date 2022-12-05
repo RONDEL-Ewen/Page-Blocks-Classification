3a168f55145502a8551964102525b9c97f615cab
RONDEL-Ewen <94047153+RONDEL-Ewen@users.noreply.github.com>
2022-12-05 21:17:10 +0100

    Delete API directory

--- API/pythondata_API/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'api'
-
-urlpatterns = [
-        path('', views.home, name='home'),
-        path('<int:pk>/model_results', views.model_results, name='model_results')
-]
